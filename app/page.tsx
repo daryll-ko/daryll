81aecb3168d72d0091ad00323741df3baf1a771a
Daryll Ko <daryll.ko@gmail.com>
2024-08-01 18:07:05 +0800

    update: give main page proper text

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@ import JP from "@/components/JP";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <JP className="text-4xl">
-        <ruby>
-          過去<rt>きのう</rt>
-        </ruby>
-        と
-        <ruby>
-          未来<rt>あした</rt>
-        </ruby>
-        、その境界線に立つ選択の数々。
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
+      <p className="text-xl">
+        Hello! I&apos;m <span className="font-bold">Daryll</span>.
+      </p>
+      <JP className="text-xl">
+        こんにちは！<span className="font-bold">ゆい</span>
+        と言います。このサイトへようこそ！
       </JP>
     </main>
   );
