a7cd0e8c51e479e2ab189a5124b05adf51075959
Daryll Ko <daryll.ko@gmail.com>
2024-08-02 19:40:01 +0800

    update: use yellow text instead of bold font

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
       <p className="text-xl">
-        Hello! I&apos;m <span className="font-bold">Daryll</span>.
+        Hello! I&apos;m <span className="text-yellow-200">Daryll</span>.
       </p>
       <JP className="text-xl">
-        こんにちは！<span className="font-bold">ゆい</span>
+        こんにちは！<span className="text-yellow-200">ゆい</span>
         と言います。このサイトへようこそ！
       </JP>
     </main>
