aa6356a5cac53efdeaeb9054dd6a6e4388b1be96
Daryll Ko <daryll.ko@gmail.com>
2024-09-04 15:38:36 +0800

    add: include slug list

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -1,13 +1,23 @@
-import JP from "@/components/JP";
+import { getSlugs } from "@/lib/fileOps";
+import Link from "next/link";
+
+function getLinks() {
+  const slugs = getSlugs();
+
+  return slugs.map((slug, index) => (
+    <Link href={`/journal/${slug}`} key={index}>
+      <p className="rounded-md text-lg hover:bg-gray-700 px-4 py-2">{slug}</p>
+    </Link>
+  ));
+}
 
 export default function Journal() {
+  const links = getLinks();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
-      <h1 className="text-xl">Journal</h1>
-      <JP className="text-xl">
-        こんにちは！<span className="text-yellow-200">ゆい</span>
-        と言います。このサイトへようこそ！
-      </JP>
+      <h1 className="text-3xl">Journal</h1>
+      <div className="flex flex-col gap-1 w-[50%]">{links}</div>
     </main>
   );
 }
