c806c32c69e1f9f302283029e5b8ca21eaa13fb5
Daryll Ko <daryll.ko@gmail.com>
2024-09-08 15:50:01 +0800

    update: fix homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import JP from "@/components/JP";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
-      <p className="text-xl">
+    <div className="flex h-80 flex-col items-center justify-center text-2xl gap-5">
+      <p>
         Hello! I&apos;m <span className="text-yellow-200">Daryll</span>.
       </p>
-      <JP className="flex flex-col items-center md:block text-xl">
+      <JP className="flex flex-col items-center md:block">
         <span>
           こんにちは！<span className="text-yellow-200">ゆい</span>
           と言います。
         </span>
         <span>このサイトへようこそ！</span>
       </JP>
-    </main>
+    </div>
   );
 }
