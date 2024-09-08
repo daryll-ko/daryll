dfb652bcd97b368a2e941b8632fc9d53f47b60f7
Daryll Ko <daryll.ko@gmail.com>
2024-09-08 14:34:19 +0800

    update: make chat page responsive

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="mt-52 mb-28 flex flex-row items-center justify-center gap-10">
+    <main className="mt-52 mb-28 px-10 md:px-0 flex flex-row items-center justify-center gap-10">
       {children}
     </main>
   );
